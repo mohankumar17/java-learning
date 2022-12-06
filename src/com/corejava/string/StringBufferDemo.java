2bcf2fb18e7e4b287a53101f50b95567efc26b4f
mohankumar17 <kothalamohankumar17@gmail.com>
2022-12-06 11:56:17 +0530

    String classes added

---
+++ src/com/corejava/string/StringBufferDemo.java
@@ -0,0 +1,32 @@
+package com.corejava.string;
+
+public class StringBufferDemo {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Tour"); 
+		//default capacity - 16, so newCapacity - 16 + 4(Tour) = 20
+		System.out.println(sb.capacity());
+		sb.append("T");
+		sb.append("rip");
+		System.out.println(sb);
+		System.out.println(sb.length());
+		
+		//capacity = 20
+		sb.append("WelcomeToAustralia");
+		//capacity = (oldCapacity*2)+2 = (20*2)+2 = 42
+		System.out.println(sb.length());
+		System.out.println(sb.capacity());
+		
+		System.out.println(sb);
+		sb.insert(15, "All");
+		System.out.println(sb);
+		
+		sb.replace(4, 8, "-");
+		System.out.println(sb);
+		
+		sb.delete(12, 15);
+		System.out.println(sb);
+		
+	}
+
+}
--- src/com/corejava/string/StringDemo.java
+++ src/com/corejava/string/StringDemo.java
@@ -3,8 +3,43 @@ package com.corejava.string;
 public class StringDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s1 = "Tour";
+		System.out.println(s1);
+		System.out.println(s1.length());
+		System.out.println(s1.charAt(2));
+		System.out.println(s1.startsWith("co"));
+		System.out.println(s1.endsWith("ur"));
+		System.out.println(s1.indexOf("u"));
 
+		String s2 = "Tour";
+		String s3 = "tour";
+		String s4 = "trip";
+
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s4);
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equals(s4));
+		System.out.println(s1.equalsIgnoreCase(s3));
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s4.compareTo(s3));
+		System.out.println(s4.toUpperCase());
+		System.out.println(s3.concat(s4));
+
+		String s5 = new String("Tour");
+		String s6 = new String("Tour");
+		String s7 = new String("Trip");
+		String s8 = new String("AXE");
+
+		System.out.println(s5 == s6);
+		System.out.println(s5 == s7);
+		System.out.println(s5.equals(s6));
+		System.out.println(s5.equals(s7));
+		System.out.println(s5.compareTo(s6));
+		System.out.println(s8.toLowerCase());
+
+		String[] list = { "ABC", "DEF", "GHI" };
+		String jStr = String.join("-", list);
+		System.out.println(jStr);
 	}
 
 }
